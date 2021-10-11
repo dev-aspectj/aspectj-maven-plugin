ff81a078d1e8d9cb9320f1742a21e24c99ba8041
Alexander Kriegisch <Alexander@Kriegisch.name>
2021-10-11 11:35:45 +0200

    Update GH pages to plugin version 1.13.1

--- apidocs/package-search-index.js
+++ apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.codehaus.mojo.aspectj"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.codehaus.mojo.aspectj"}]
